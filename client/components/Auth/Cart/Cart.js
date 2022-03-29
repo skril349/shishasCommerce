7eaaea0c11e021965a26672a180729b1af3ac441
toniprotofy <toni@protofy.xyz>
2022-03-29 14:42:23 +0200

    creating component Cart

--- client/components/Auth/Cart/Cart.js
+++ client/components/Auth/Cart/Cart.js
@@ -1,11 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../../layout/Layout";
 import useCart from "../../../hooks/useCart";
 export default function Cart() {
-  console.log(useCart());
+  const { getProductsCart } = useCart();
+  const products = getProductsCart();
+  const [productsData, setProductsData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const productsTemp = [];
+    })();
+  }, []);
+
+  console.log(products);
+  return products ? <FullCart products={products} /> : <EmptyCart />;
+}
+
+function EmptyCart() {
   return (
     <Layout>
-      <div className="cart"></div>
+      <div className="cart">
+        <div className="empty-cart">
+          <h1>NO HAY COSAS EN EL CARRITO</h1>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+function FullCart(props) {
+  const { products } = props;
+  return (
+    <Layout>
+      <div className="cart">
+        <div className="full-cart">
+          <h1>Carrito</h1>
+        </div>
+      </div>
     </Layout>
   );
 }
