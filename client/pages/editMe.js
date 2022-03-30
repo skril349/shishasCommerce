93a2c27af658f8b0c5ae5e39d8fa655c2e481cfa
toniprotofy <toni@protofy.xyz>
2022-03-30 13:50:58 +0200

    adding editMe page

---
+++ client/pages/editMe.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CartPage from "../components/Auth/Cart";
+import Edit from "../components/Auth/EditMe";
+export default function EditMe() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Edit />
+    </div>
+  );
+}
