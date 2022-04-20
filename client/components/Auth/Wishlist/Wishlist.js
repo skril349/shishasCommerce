8f94003bec8d133f93d693b87cd781872ce57672
toniprotofy <toni@protofy.xyz>
2022-04-20 16:05:53 +0200

    creating wishlist form layout

--- client/components/Auth/Wishlist/Wishlist.js
+++ client/components/Auth/Wishlist/Wishlist.js
@@ -4,7 +4,14 @@ import Layout from "../../../layout/Layout";
 export default function Wishlist() {
   return (
     <Layout>
-      <div fluid className="wishlist"></div>
+      <div fluid className="wishlist">
+        <div className="wishlist__block">
+          <div className="title">LISTA DE DESEOS</div>
+          <div className="data">
+            <p>LISTA DE PRODUCTOS</p>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
